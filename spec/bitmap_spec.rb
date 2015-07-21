b4edf54ad6d4d5db10ad2b9b12bd90bed01ff954
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-21 22:53:47 +0200

    Fix width/height test and add rgb check.

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -2,8 +2,15 @@ require 'bitmap'
 
 RSpec.describe Bitmap, "#init" do
   context "With a given width and height" do
-    it "initializes a 2D array with random RGB values"
-    bitmap = Bitmap.new
-    expect(bitmap.data[0]).to eq 1
+    width, height = 2, 2
+    it "initializes a 2D array with given width and height" do
+      bitmap = Bitmap.new width, height
+      expect(bitmap.data.length).to eq width
+      expect(bitmap.data[0].length).to eq height
+    end
+    it "initializes a 2D array with random RGB values" do
+      bitmap = Bitmap.new width, height
+      expect(bitmap.data[0][0].length).to eq 3
+    end
   end
 end
