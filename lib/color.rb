34946928de920003e68241c0a28cc21574b2b03d
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-22 22:46:26 +0200

    Add color implementation.

---
+++ lib/color.rb
@@ -0,0 +1,15 @@
+
+# RGB Color
+class Color
+  attr_reader :r, :g, :b
+
+  def initialize(r, g, b)
+    @r, @g, @b = r, g, b
+  end
+end
+
+class RandomColor < Color
+  def initialize
+    super rand(255), rand(255), rand(255)
+  end
+end
