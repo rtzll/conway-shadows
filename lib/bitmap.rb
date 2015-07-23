05a351311f2f74c53184698eca26ee812d97d720
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-23 22:30:30 +0200

    Start implementing enlarge, still failing.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -5,16 +5,26 @@ require 'color'
 class Bitmap
   attr_reader :data
 
-  def initialize(width, height)
+  def initialize(width, height, color=nil)
     @data = Array.new(width) do
       Array.new(height) do
-        RandomColor.new
+        color || RandomColor.new
       end
     end
   end
 
   # Enlarge the data by a given factor.
   def enlarge(factor)
-    @data * factor
+    enlarged_data = Array.new(factor * @data[0].length) { [] }
+    @data.each_with_index do |row, row_index|
+      shift = 0
+      0.upto(factor) do |i|
+        factor.times {enlarged_data[row_index+shift] << row }
+        shift += i
+      end
+    end
+    puts enlarged_data[0].to_s
+    puts enlarged_data[0].length
+    @data = enlarged_data
   end
 end
