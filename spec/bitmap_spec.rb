7d7d13ca909a906df2b776b15c002011b995be7d
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-23 22:30:12 +0200

    Add test for enlarge

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -15,3 +15,20 @@ RSpec.describe Bitmap, "#init" do
     end
   end
 end
+
+
+RSpec.describe Bitmap, "#enlarge" do
+  context "With a given width, height and color" do
+    width, height, factor = 2, 2, 2
+    color = Color.new 1, 2, 3
+    it "enlarging a 2x2 bitmap by 2 will yield a 4x4 bitmap." do
+      bitmap = Bitmap.new width, height, color
+      bitmap.enlarge 2
+      # check length
+      expect(bitmap.data.length).to eq width*factor
+      expect(bitmap.data[0].length).to eq height*factor
+      # check values
+      expect(bitmap.data[0]).to eq [color, color] * factor
+    end
+  end
+end
