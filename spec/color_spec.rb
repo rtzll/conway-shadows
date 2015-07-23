f15cca58c640e1a4608845c413d73af7d68d7d7d
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-23 22:29:47 +0200

    Test to_s

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -1,5 +1,12 @@
 require 'color'
 
+RSpec.describe Color, "#to_s" do
+  it "converts the color to a string with r, g, b (in that order)" do
+    color = Color.new 1, 2, 3
+    expect(color.to_s).to eq "1, 2, 3"
+  end
+end
+
 RSpec.describe RandomColor, "#init" do
   it "initializes a rgb color with values between 0 and 255" do
     color = RandomColor.new
