0dc38cdb4ee2566479890fffae2a3230cd7603d8
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-24 23:44:54 +0200

    Improve readability.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -16,7 +16,7 @@ class Bitmap
   # Enlarge the data by a given factor.
   def enlarge(factor)
     widened_data = stretch_width @data[0].length, factor
-    @data  = stretch_height @data.length, factor, widened_data
+    @data = stretch_height @data.length, factor, widened_data
   end
 
   def stretch_width(width, factor)
@@ -31,7 +31,7 @@ class Bitmap
 
   def stretch_height(height, factor, data)
     stretched = Array.new(height) { [] }
-    0.upto(factor*height - 1) do |index|
+    (factor*height).times do |index|
       stretched[index] = data[index/factor]
     end
     stretched
