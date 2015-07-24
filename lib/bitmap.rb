8adf9305519440bbfd6be7b7fae690f0142783b1
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-24 23:35:45 +0200

    Refactored enlarge into sub steps: stretch width, height.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -15,15 +15,26 @@ class Bitmap
 
   # Enlarge the data by a given factor.
   def enlarge(factor)
-    enlarged_data = Array.new(@data[0].length) { [] }
+    widened_data = stretch_width @data[0].length, factor
+    @data  = stretch_height @data.length, factor, widened_data
+  end
+
+  def stretch_width(width, factor)
+    stretched = Array.new(width) { [] }
     @data.each_with_index do |row, row_index|
       row.each do |entry|
-        factor.times { enlarged_data[row_index] << entry }
+        factor.times { stretched[row_index] << entry }
       end
     end
-    0.upto(factor*@data.length - 1) do |index|
-      @data[index] = enlarged_data[index/factor]
+    stretched
+  end
+
+  def stretch_height(height, factor, data)
+    stretched = Array.new(height) { [] }
+    0.upto(factor*height - 1) do |index|
+      stretched[index] = data[index/factor]
     end
+    stretched
   end
 
 end
