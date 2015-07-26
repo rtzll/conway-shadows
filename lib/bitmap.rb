bf326e5b3edd155a1a8ffad850fa4b2f44d782f4
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-26 22:18:06 +0200

    Make width and height class attributes.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -3,11 +3,12 @@ require 'color'
 # The bitmap object.
 # A 2D array with rgb values as entries.
 class Bitmap
-  attr_reader :data
+  attr_reader :data, :width, :height
 
   def initialize(width, height)
-    @data = Array.new(width) do
-      Array.new(height) do
+    @width, @height = width, height
+    @data = Array.new(@width) do
+      Array.new(@height) do
         RandomColor.new
       end
     end
@@ -20,23 +21,21 @@ class Bitmap
   end
 
   def stretch_width(factor)
-    width = @data[0].length
-    stretched = Array.new(width) { [] }
+    stretched = Array.new(@width) { [] }
     @data.each_with_index do |row, row_index|
       row.each do |entry|
         factor.times { stretched[row_index] << entry }
       end
     end
-    @data = stretched
+    @data, @width = stretched, @width*factor
   end
 
   def stretch_height(factor)
-    height = @data.length
-    stretched = Array.new(height) { [] }
-    (factor*height).times do |index|
+    stretched = Array.new(@height) { [] }
+    (factor*@height).times do |index|
       stretched[index] = @data[index/factor]
     end
-    @data = stretched
+    @data, @height = stretched, @height*factor
   end
 
 end
