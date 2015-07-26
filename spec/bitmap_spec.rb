fed121ecfb24db4c42f76fcae5efe15b807f1782
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-07-26 22:18:33 +0200

    Adjust tests to be more readable.

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -12,11 +12,15 @@ describe Bitmap do
 
   describe "when initialized" do
     it "should have the correct width and height" do
-      expect(@bitmap.data.length).to eq width
-      expect(@bitmap.data[0].length).to eq height
+      expect(@bitmap.width).to eq width
+      expect(@bitmap.height).to eq height
     end
     it "should have random color entries" do
-      expect(@bitmap.data[0][0]).to be_a RandomColor
+      @bitmap.data.each do |row|
+        row.each do |entry|
+          expect(entry).to be_a RandomColor
+        end
+      end
     end
   end
 
@@ -25,9 +29,9 @@ describe Bitmap do
       @bitmap.enlarge factor
     end
     it "should be factor times greater in width and height." do
-      expect(@bitmap.data.length).to eq height*factor
+      expect(@bitmap.height).to eq height*factor
       (height*factor).times do |index|
-        expect(@bitmap.data[index].length).to eq width*factor
+        expect(@bitmap.width).to eq width*factor
       end
     end
   end
@@ -37,9 +41,9 @@ describe Bitmap do
       @bitmap.stretch_width factor
     end
     it "should be factor times greater in width and the same in height" do
-      expect(@bitmap.data.length).to eq height
+      expect(@bitmap.height).to eq height
       (height).times do |index|
-        expect(@bitmap.data[index].length).to eq width*factor
+        expect(@bitmap.width).to eq width*factor
       end
     end
   end
@@ -49,9 +53,9 @@ describe Bitmap do
       @bitmap.stretch_height factor
     end
     it "should be factor times greater in height and the same in width" do
-      expect(@bitmap.data.length).to eq height*factor
+      expect(@bitmap.height).to eq height*factor
       (height*factor).times do |index|
-        expect(@bitmap.data[index].length).to eq width
+        expect(@bitmap.width).to eq width
       end
     end
   end
