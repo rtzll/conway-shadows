a83e1a61a61cfa922acc6e5460bfa9207eb4c41e
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-08-02 17:12:07 +0200

    Fix separator check and minor cleanup.

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -73,24 +73,23 @@ describe Bitmap do
     end
   end
 
-  def is_separator(index, every_n_pixels)
-    index % every_n_pixels + 1 == 0
-  end
-
   describe "when a separator is added" do
     white_pixel = Color.new 255, 255, 255
-    n = 1
+    $every_n_pixels = 1
     before(:each) do
-      @bitmap.add_separator n, white_pixel
+      @bitmap.add_separator $every_n_pixels, white_pixel
     end
     it "should have been stretched in heigth and width" do
-      expect(@bitmap.width).to be eq width * n - 1
-      expect(@bitmap.width).to be eq height * n - 1
+      expect(@bitmap.width).to eq(width * $every_n_pixels - 1)
+      expect(@bitmap.width).to eq(height * $every_n_pixels - 1)
+    end
+    def separator?(index)
+      index % $every_n_pixels + 1 == 0
     end
     it "should have a white pixels every n pixels" do
-      @bitmap.each_with_index do |row, row_index|
+      @bitmap.data.each_with_index do |row, row_index|
         row.each_with_index do |entry, column_index|
-          if is_separator row_index or is_separator column_index
+          if separator? row_index or separator? column_index
             expect(entry).to be eq white_pixel
           end
         end
