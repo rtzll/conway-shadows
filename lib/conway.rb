781800f8735b770e3b5706d9cae8e251d4c43848
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-08-17 22:16:16 +0200

    Fix whitespace, once again ...

--- lib/conway.rb
+++ lib/conway.rb
@@ -14,20 +14,20 @@ class Conway
 end
 
 class Grid
-  attr_reader :width, :heigth , :fields
+  attr_reader :width, :heigth, :fields
 
   def initialize(width, heigth)
     @width, @heigth = width, heigth
-		@fields = Array.new(width) do
-			Array.new(heigth) do
-				Cell.new
-			end
-		end
+    @fields = Array.new(width) do
+      Array.new(heigth) do
+        Cell.new
+      end
+    end
   end
 
-	def each
-		fields.each
-	end
+  def each
+    fields.each
+  end
 end
 
 class Cell
